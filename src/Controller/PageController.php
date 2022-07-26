3c8d07c109ff6bf821a2890e904b8d699da24ebc
Hyxotis <chris.chich@hotmail.fr>
2022-07-26 23:45:11 +0200

    :lipstick: Added "sophrologie" page

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -13,4 +13,34 @@ class PageController extends AbstractController
     {
         return $this->render('index.html.twig');
     }
+
+    #[Route('/sophrologie', name: 'app_sophrologie')]
+    public function sophrologie(): Response
+    {
+        return $this->render('sophrologie.html.twig');
+    }
+
+    #[Route('/seances', name: 'app_seances')]
+    public function seances(): Response
+    {
+        return $this->render('sophrologie.html.twig');
+    }
+
+    #[Route('/qui-suis-je', name: 'app_qui_suis_je')]
+    public function quiSuisJe(): Response
+    {
+        return $this->render('sophrologie.html.twig');
+    }
+
+    #[Route('/tarifs', name: 'app_tarifs')]
+    public function tarifs(): Response
+    {
+        return $this->render('sophrologie.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('sophrologie.html.twig');
+    }
 }
