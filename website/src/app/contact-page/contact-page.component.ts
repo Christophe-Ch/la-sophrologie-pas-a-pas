9f1bdb8b906ab543353061d8445d101766f9e9d9
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2023-03-12 16:15:08 +0100

    :lipstick: Added contact page template

--- website/src/app/contact-page/contact-page.component.ts
+++ website/src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Feature, Map, View } from 'ol';
+import { Point } from 'ol/geom';
+import TileLayer from 'ol/layer/Tile';
+import * as layer from 'ol/layer';
+import * as source from 'ol/source';
+import { fromLonLat } from 'ol/proj';
+import OSM from 'ol/source/OSM';
+import Icon from 'ol/style/Icon';
+import Style from 'ol/style/Style';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,10 +15,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.scss']
 })
 export class ContactPageComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit(): void {
-  }
+    const map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: fromLonLat([1.351260, 49.503270]),
+        zoom: 13
+      })
+    });
 
+    const markers = new layer.Vector({
+      source: new source.Vector(),
+      style: new Style({
+        image: new Icon({
+          anchor: [0.5, 1],
+          src: '/assets/marker.png',
+          scale: 0.5
+        })
+      })
+    });
+    map.addLayer(markers);
+
+    const marker = new Feature(new Point(fromLonLat([1.351260, 49.503270])));
+    markers.getSource()?.addFeature(marker);
+  }
 }
