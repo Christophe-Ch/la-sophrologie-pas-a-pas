5f234551dfdda6637fd2d027e6882914cd46658a
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2024-10-05 16:14:00 +0200

    :lipstick: Add session type submenu

--- website/src/app/menu/menu.component.ts
+++ website/src/app/menu/menu.component.ts
@@ -5,17 +5,27 @@ import { filter } from 'rxjs';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
   @ViewChild('close') private _closeToggle!: ElementRef;
+  @ViewChild('submenuToggle') private _submenuToggle!: ElementRef;
 
-  constructor(private _router: Router) { }
+  constructor(private _router: Router) {}
 
   ngOnInit(): void {
     this._router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe(() => { (this._closeToggle.nativeElement as HTMLInputElement).checked = false; });
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        (this._closeToggle.nativeElement as HTMLInputElement).checked = false;
+      });
   }
 
+  showSubmenu(): void {
+    (this._submenuToggle.nativeElement as HTMLInputElement).checked = true;
+  }
+
+  closeSubmenu(): void {
+    (this._submenuToggle.nativeElement as HTMLInputElement).checked = false;
+  }
 }
