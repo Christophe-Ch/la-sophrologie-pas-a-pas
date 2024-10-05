2f851416672d0058515fdae85cb1096dc9960dd8
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2024-10-05 17:14:12 +0200

    :sparkles: Improve SEO

--- website/src/app/home-page/home-page.component.ts
+++ website/src/app/home-page/home-page.component.ts
@@ -5,32 +5,38 @@ import { TitleService } from '../title.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  constructor(private readonly _titleService: TitleService, private readonly _meta: Meta) { }
+  constructor(
+    private readonly _titleService: TitleService,
+    private readonly _meta: Meta
+  ) {}
 
   ngOnInit(): void {
     this._titleService.setTitle('Sophrologue');
     this._meta.updateTag({
       name: 'description',
-      content: 'Découvrez les séances de sophrologie proposées par Anne Avenel Dubois, sophrologue basée sur Saint-Aignan-Sur-Ry. Je propose des séances pour tous les âges et besoins pour vous aider à gérer votre stress, retrouver votre équilibre et vous reconnecter à vous-même. Contactez-moi pour prendre rendez-vous dès maintenant.'
+      content:
+        'Découvrez la sophrologie avec Anne Avenel Dubois. Séances personnalisées pour gérer le stress et améliorer votre bien-être.',
     });
     this._meta.updateTag({
       name: 'og:description',
-      content: 'Découvrez les séances de sophrologie proposées par Anne Avenel Dubois, sophrologue basée sur Saint-Aignan-Sur-Ry. Je propose des séances pour tous les âges et besoins pour vous aider à gérer votre stress, retrouver votre équilibre et vous reconnecter à vous-même. Contactez-moi pour prendre rendez-vous dès maintenant.'
+      content:
+        'Découvrez la sophrologie avec Anne Avenel Dubois. Séances personnalisées pour gérer le stress et améliorer votre bien-être.',
     });
     this._meta.updateTag({
       name: 'og:image',
-      content: '/assets/home.jpg'
+      content: '/assets/home.jpg',
     });
     this._meta.updateTag({
       name: 'keywords',
-      content: 'Sophrologie, séances de sophrologie, Saint-Aignan-Sur-Ry, Anne Avenel Dubois, contact, rendez-vous, demande de renseignements, qualité de vie'
+      content:
+        'Sophrologie, séances de sophrologie, Saint-Aignan-Sur-Ry, Anne Avenel Dubois, contact, rendez-vous, demande de renseignements, qualité de vie',
     });
     this._meta.updateTag({
       name: 'twitter:card',
-      content: 'summary'
+      content: 'summary',
     });
   }
 }
